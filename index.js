10668734bfa666a0ffda9cdec6bf6137006b7f07
Ajay Kanjilal <ajaykanjilal1122@gmail.com>
2023-07-03 14:38:56 +0530

    Added methods and content-type in res.header

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
   next();
 });
 
