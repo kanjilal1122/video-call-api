e1205fa8e40503f11c0ee6aa9f43cfe923872e35
Ajay Kanjilal <ajaykanjilal1122@gmail.com>
2023-07-11 17:06:07 +0530

    refactor code

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const uuidv1 = require("uuid");
 //const bodyParser = require(body - parser);
-let meetingId = 1;
+let meeting_id = 1;
 
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   if (req.method === "POST") {
-    meetingId++;
+    meeting_id++;
   }
   next();
 });
@@ -36,126 +36,124 @@ app.post("/", (req, res) => {
 
   //const callback_url = "http://localhost:5173/thankyou";
   //const time_stamp = Date.now();
-  const uuid_id = uuidv1.v1();
-  const meetingLink = `https://vision.proteger.one/meeting/${uuid_id}`;
-  const dev_meetingLink = `localhost:5173/meeting/${uuid_id}`;
-  const call_back_url_res = callback_url;
-  const recording_status_res = recording_status;
-  console.log(call_back_url_res, recording_status_res);
+  const Meeting_uuid = uuidv1.v1();
+  const meeting_link = `https://vision.proteger.one/meeting/${Meeting_uuid}`;
+  const dev_meeting_link = `localhost:5173/meeting/${Meeting_uuid}`;
+  //const callback_url = callback_url;
+  //const recording_status_res = recording_status;
+  console.log(callback_url, recording_status);
 
   res.json({
-    meetingLink,
-    dev_meetingLink,
-    meeting_name: meeting_name,
-    Meeting_UUID: uuid_id,
+    meeting_link,
+    dev_meeting_link,
+    meeting_name,
+    Meeting_uuid,
     //create_at: time_stamp,
     //callback_url: callback_url,
-    meetingId: meetingId,
+    meeting_id
+    
   });
 });
 
 app.get("/meeting_details", (req, res) => {
   const meeting_id = 12345;
-  const meeting_uuid_id = uuidv1.v1();
-  const meetingLink = `https://vision.proteger.one/meeting/${meeting_uuid_id}`;
+  const meeting_uuid = uuidv1.v1();
+  const meeting_link = `https://vision.proteger.one/meeting/${meeting_uuid}`;
   const meeting_name = "geogo";
   //const dev_meetingLink = `localhost:5173/meeting/${uuid_id}`;
   res.json({
-    meetingLink,
+    meeting_link,
     meeting_name,
-    meeting_uuid_id,
+    meeting_uuid,
     meeting_id,
   });
 });
 
-//post / create_meeting 
+//post / create_meeting
 app.get("/create_meeting", (req, res) => {
   const meeting_id = 12345;
-  const meeting_uuid_id = uuidv1.v1();
-  //const meetingLink = `https://vision.proteger.one/meeting/${meeting_uuid_id}`;
+  const meeting_uuid = uuidv1.v1();
+  const meeting_link = `https://www.google.come`;
   const meeting_name = "geogo";
-  
+
   res.json({
-    
     meeting_name,
-    meeting_uuid_id,
+    meeting_uuid,
     meeting_id,
+    meeting_link,
   });
 });
-
-
 
 app.post("/create_meeting", (req, res) => {
   // const { name, id } = req.body;
   const { meeting_name, callback_url, recording_status } = req.body;
+  //const callback_url = callback_url;
+  //const recording_status_res = recording_status;
+  console.log(callback_url, recording_status);
 
   //const callback_url = "http://localhost:5173/thankyou";
   //const time_stamp = Date.now();
-  //const uuid_id = uuidv1.v1();
-  //const meetingLink = `https://vision.proteger.one/meeting/${uuid_id}`;
+  const meeting_uuid = uuidv1.v1();
+  const meeting_link = `https://vision.proteger.one/meeting/${meeting_uuid}`;
   //const dev_meetingLink = `localhost:5173/meeting/${uuid_id}`;
-  const call_back_url_res = callback_url;
-  const recording_status_res = recording_status;
-  console.log(call_back_url_res, recording_status_res);
 
   res.json({
-    // meetingLink,
+    callback_url,
+    recording_status,
+    meeting_name,
+    meeting_link,
+    meeting_uuid,
+
     // dev_meetingLink,
     // meeting_name: meeting_name,
-    // Meeting_UUID: uuid_id,
     //create_at: time_stamp,
-    // callback_url: call_back_url_res,
+    // callback_url: callback_url_res,
     // meetingId: meetingId,
-    call_back_url_res ,
-    recording_status_res ,
-    meeting_name
-
   });
 });
 
+// app.get("/test", (req, res) => {
+//   const dateTime = new Date();
+//   const userId = uuidv1.v1();
+//   //console.log("UserId", userId); `This is a get method for  test route  ,  datetime is  ${dateTime} and UserId is ${userId} `
+//   res.json({
+//     date: dateTime,
+//     user: userId,
+//   });
+// });
 
-app.get("/test", (req, res) => {
-  const dateTime = new Date();
-  const userId = uuidv1.v1();
-  //console.log("UserId", userId); `This is a get method for  test route  ,  datetime is  ${dateTime} and UserId is ${userId} `
-  res.json({
-    date: dateTime,
-    user: userId,
-  });
-});
+// app.post("/test", (req, res) => {
+//   const { dateTime, name, userId } = req.body;
+//   const time = new Date().toUTCString();
+//   //console.log(time); `Name is :${name} , UserId : ${userId}  ,  dateTime ${dateTime} and time ${time} `
 
-app.post("/test", (req, res) => {
-  const { dateTime, name, userId } = req.body;
-  const time = new Date().toUTCString();
-  //console.log(time); `Name is :${name} , UserId : ${userId}  ,  dateTime ${dateTime} and time ${time} `
+//   res.json({
+//     Name: name,
+//     DateTime: dateTime,
+//     userId: userId,
+//   });
+// });
 
-  res.json({
-    Name: name,
-    DateTime: dateTime,
-    userId: userId,
-  });
-});
+// app.get("/api/users", (req, res) => {
+//   const { user_id, token, meetinglink } = req.query;
+//   res.json({
+//     "user Id ": user_id,
+//     "Token ": token,
+//     "MeetingLink:": meetinglink,
+//   });
+// });
 
-app.get("/api/users", (req, res) => {
-  const { user_id, token, meetinglink } = req.query;
-  res.json({
-    "user Id ": user_id,
-    "Token ": token,
-    "MeetingLink:": meetinglink,
-  });
-});
+// //target specificy a params using params functiion and this is the middleware
+// app.param("name", (req, res, next, name) => {
+//   const changeName = name.toUpperCase();
+//   req.name = changeName;
+//   next();
+// });
 
-//target specificy a params using params functiion and this is the middleware
-app.param("name", (req, res, next, name) => {
-  const changeName = name.toUpperCase();
-  req.name = changeName;
-  next();
-});
-
-app.get("/api/users/:name", (req, res) => {
-  console.log(req.nam);
-  res.json("Hello " + req.name + "!!!");
-});
+// app.get("/api/users/:name", (req, res) => {
+//   console.log(req.nam);
+//   res.json("Hello " + req.name + "!!!");
+// });
 
 // app.get("/api/:version", (req, res) => {
 //   res.json({
